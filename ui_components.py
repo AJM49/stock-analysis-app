f4fdbe1c4d98a36d692841939021fd3a464392ac
Albe Murp <albemurp@Reginas-MacBook-Air-3.local>
2026-06-12 02:42:20 -0400

    Move sidebar UI into sidebar module

--- ui_components.py
+++ ui_components.py
@@ -11,7 +11,7 @@ from ui.charts import render_price_chart
 from ui_components_legacy_backup import make_arrow_safe
 from ui.stock_views import render_company_profile
 from ui.portfolio_views import render_portfolio_dashboard
-from ui_components_legacy_backup import render_portfolio_sidebar
+from ui.sidebar import render_portfolio_sidebar
 from ui.portfolio_views import render_portfolio_table
 from ui.portfolio_views import render_risk_dashboard
 from ui_components_legacy_backup import render_stock_comparison
@@ -19,4 +19,4 @@ from ui.stock_views import render_stock_export
 from ui.stock_views import render_stock_header
 from ui.portfolio_views import render_stop_loss_calculator
 from ui.stock_views import render_technical_indicators
-from ui_components_legacy_backup import render_watchlist_sidebar
+from ui.sidebar import render_watchlist_sidebar
